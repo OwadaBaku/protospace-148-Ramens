9a85679a14f5dd7de67a64cd69e09f8a43b287b0
teouoo <happpyprogrammming@gmail.com>
2023-05-19 18:16:42 +0900

    バリデーションの設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,5 +2,10 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  has_one_attached :image 
+  has_one_attached :image
+  
+  validates :name, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
 end
