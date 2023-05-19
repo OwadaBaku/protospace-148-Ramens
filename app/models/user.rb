6d511be3630f01bf40a71d649aa23c04cbef8972
teouoo <happpyprogrammming@gmail.com>
2023-05-19 15:49:26 +0900

    モデルで誤字修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :profile, presence: true
-  validates :oppupation, presence: true
+  validates :occupation, presence: true
   validates :position, presence: true
 end
