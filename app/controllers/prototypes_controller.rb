ee03afcca7a4ee828c07e08611b965456835a267
teouoo <happpyprogrammming@gmail.com>
2023-05-20 10:54:19 +0900

    プロトタイプ一覧機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,15 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user).order('created_at DESC')
   end
 
   def new
     @prototype = Prototype.new
   end
-  
+
   def create
     @prototype = Prototype.new(prototype_params)
-  
+
     if @prototype.save
       # 保存に成功した場合の処理
       redirect_to root_path
@@ -23,5 +24,4 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
 end
