7e571664ec00713f279432e75377db8141777b1c
teouoo <happpyprogrammming@gmail.com>
2023-05-22 16:45:31 +0900

    ログイン状態での動作変更実装
    ログイン状態の場合でも、自身が投稿していないプロトタイプのプロトタイプ情報編集ページへ遷移しようとすると、トップページに遷移する

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, only: [:edit]
+
   def index
     @prototypes = Prototype.includes(:user).order('created_at DESC')
   end
@@ -25,6 +27,7 @@ class PrototypesController < ApplicationController
   
   def update
     @prototype = Prototype.find(params[:id])
+
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -41,4 +44,12 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user == prototype.user
+      redirect_to action: :index
+    end
+  end
+  
 end
