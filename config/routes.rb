bd2de71dbbe4a26fb2d97e91a99c77be561cdfa5
OwadaBaku <82127461+OwadaBaku@users.noreply.github.com>
2023-05-22 17:43:13 +0900

    usersコントローラーにshowアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :destroy, :edit, :update]
+  resources :users, only: [:show]
 end
