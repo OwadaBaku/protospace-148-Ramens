ab360981e40eabaa56eeff224c20d3f69b23a857
teouoo <happpyprogrammming@gmail.com>
2023-05-22 17:43:21 +0900

    コントローラー、アクション、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :destroy, :edit, :update]
+
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
